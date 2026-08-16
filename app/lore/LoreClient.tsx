b4f1108b2815bb0892735aca98298e4a680650c8
Alex Shibu <alexshibu04@gmail.com>
2026-08-15 20:16:46 -0400

    a few changes

--- app/lore/LoreClient.tsx
+++ app/lore/LoreClient.tsx
@@ -85,6 +85,7 @@ function inferSortKey(date: string): number {
   return 0;
 }
 
+
 const LORE: LoreItem[] = [
   {
     id: "jersey-importer-2026-06",
@@ -92,12 +93,30 @@ const LORE: LoreItem[] = [
     line: "Chinese Jersey importer for the World Cup",
     more: "Became a Chinese jersey importer for the World Cup. Facetimed factories, learned enough Mandarin to negotiate, got custom pieces made at prices that shouldn't exist, and figured out customs. Talked with 15+ suppliers evryone from eletronics to peptides. I came out knowing how relationships get built in this industry ",
   },
+    {
+    id: "syro-shark-2026-07",
+    date: "07.2026",
+    line: "Pitched at Syro Shark in Chicago",
+    more: "I got the opportunity to go for the Silver Jubilee of the Chicago Syro Malabar Diocese where I pitched Avalonn for Syro Shark Tank. We didn't win. Met some really incredible people and mentors. Learned a hard lesson about what actual traction looks like.",
+
+    links: [
+      {
+        label: "Instagram",
+        url: "https://www.instagram.com/p/Dc0g1k7s2eC/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      },
+      {
+        label: "Pitch video",
+        url: "https://youtu.be/o5b-4m51GvM?si=Hwm8GFF8nGXcWsQN",
+      }
+    ]
+  },
+
   {
     id: "smym-feast-2026-07",
     date: "07.2026",
     line: "Helped 20 kids earn $3K running booths at Parish Feast",
     more:
-      "One of my favorite things I've led this year started with a question. How do we become a catalyst for young folks who want to get a glimpse of entrepreneurship. For our three-night Parish Feast which pulls around 1,500 people a night Scarborough SMYM builds a whole economy. Instead of doing the standard thing of running a  couple food stalls ourselves, we became an incubator investing 60 bucks into 8 groups of 4 kids. I set up a negative working capital model with the booths and SMYM ourselves owned high margin lanes like popcorn,  and cotton candy. Which became a financial backbone for the kids to experiment, building a whole ticket economy with fraud controls, dual ledger reconciliation all while also filming a documentary about the ups and downs of the whole 40+ person operation. Night after night of organizing PR, dealing with a chaotic mess, my job everyday was crisis management. We made over 3k in revenue and had a whole lot of fun doing it.",
+      "One of my favorite things I've led this year started with a question. How do we become a catalyst for young folks who want to get a glimpse of entrepreneurship. For our three-night Parish Feast which pulls around 1,500 people a night Scarborough SMYM builds a whole economy. Instead of doing the standard thing of running a  couple food stalls ourselves, we became an incubator investing 60 bucks into 8 groups of 4 kids. I set up a negative working capital model with the booths Building a whole ticket economy with fraud controls, dual ledger reconciliation all while also filming a documentary about the ups and downs of the whole 40+ person operation. Night after night of organizing PR, dealing with a chaotic mess, my job everyday was crisis management. We made over 3k in revenue and had a whole lot of fun doing it.",
     links: [
       {
         label: "Instagram",
